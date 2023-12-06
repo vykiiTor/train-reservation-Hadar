adf019e0c3844588542544506d16dd8dd36d01ab
vykiiTor <vykiitor@gmail.com>
2023-12-06 11:54:51 +0100

    counter sorti car plubo

--- ts/booking-reference/app.ts
+++ ts/booking-reference/app.ts
@@ -1,24 +1,12 @@
 import express from 'express'
 import morgan from 'morgan'
-
+import {Counter} from './counter'
 const port = 8082
 
 const app = express()
 app.use(morgan('tiny'))
 
-class Counter {
-  private _count: number
 
-  constructor() {
-    this._count = 123456789
-  }
-
-  increment() {
-    this._count += 1
-  }
-
-  value() { return this._count.toString() }
-}
 
 const counter = new Counter()
 
---
+++ ts/booking-reference/counter.ts
@@ -0,0 +1,15 @@
+class Counter {
+    private _count: number
+  
+    constructor() {
+      this._count = 123456789
+    }
+  
+    increment() {
+      this._count += 1
+    }
+  
+    value() { return this._count.toString() }
+  }
+
+  export {Counter}
